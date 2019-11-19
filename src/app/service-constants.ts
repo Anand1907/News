d6facabb9f1f4390813fe3ed74c3dce241f0fabf
Anand Moorthy <anandmoorthy90@gmail.com>
2019-11-19 22:38:59 +0530

    Added Utilities and Service Constants

---
+++ src/app/service-constants.ts
@@ -0,0 +1,10 @@
+
+export const NEWS = {
+    BASE_URL: "https://newsapi.org/",
+    API_KEY: "dd44057031b94e719120619236de9599"
+}
+
+export const Constants = {
+    NEWS_LIST: 'v2/top-headlines',
+    SOURCES: "bbc-news"
+  }
---
+++ src/app/utils/utils.ts
@@ -0,0 +1,14 @@
+import { ToastController } from '@ionic/angular';
+
+export class Utils {
+    constructor(private toastCtrl: ToastController) {}
+
+    async showToast(msg) {
+        let toast = await this.toastCtrl.create({
+            message: msg,
+            duration: 2000,
+            color: 'dark'
+          });
+          toast.present();
+    }
+}
